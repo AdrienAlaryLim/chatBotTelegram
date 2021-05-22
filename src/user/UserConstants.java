33f7eadd9ccf53c6ca3bbf60e0b66a14ca388e5a
AdrienAlaryLim <adrien.alary@limayrac.fr>
2021-05-22 13:56:12 +0200

    Use env values as constants

--- src/user/UserConstants.java
+++ src/user/UserConstants.java
@@ -7,7 +7,6 @@ public class UserConstants
 {
 	// Define your url to connect to your database with your database name
 	// ex : jdbc:mysql://127.0.0.1:3306/mydb_name
-	
 	private static final String sqlUrl = "jdbc:mysql://127.0.0.1:3306/mydb_name";
 	
 	// Your database user
@@ -20,11 +19,11 @@ public class UserConstants
 	
 	// Define your telegram bot token API key
 	// ex: 1234567890:MyTelegramBotAPIKeyHash
-	private static final String botToken = "1234567890:MyTelegramBotAPIKeyHash";
+	private static final String botToken = System.getenv("BOT_TOKEN");
 	
 	// Define your telegram bot username
 	// ex: MyTelegramBotUsername
-	private static final String botUsername = "MyTelegramBotUsername";
+	private static final String botUsername = System.getenv("BOT_USERNAME");
 	
 	// Define your admin telegram channels
 	// ex: Arrays.asList("channelAdmin1", "channelAdmin2")
